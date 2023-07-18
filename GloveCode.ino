9bf5dd0a05ac65b0978807553771d59c3d5d1f45
Camp Student <campstudent@SG4-3202-03.USG.LOCAL>
2023-07-18 15:55:47 -0400

    Add IMU test code
    uses Arduino Serial plotter
    no sensor fusion yet

--- GloveCode.ino
+++ GloveCode.ino
@@ -1,12 +1,39 @@
 #include <SensorFusion.h>
 #include <Adafruit_MPU6050.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 imu;
 
 #define TEST_POT 36
 
 void setup() {
     Serial.begin(115200);
+    Serial.print("Connecting IMU...         ");
+    if (!imu.begin()) {
+       Serial.println("[ FAIL ]\nFATAL ERROR: IMU disconnected!!");
+       ESP.restart();
+    } else
+        Serial.println("[  OK  ]");
+    Serial.print("Configuring IMU accel...  ");
+    imu.setAccelerometerRange(MPU6050_RANGE_8_G);
+    Serial.println("[  OK  ]");
+    Serial.print("Configuring IMU gyro...   ");
+    imu.setGyroRange(MPU6050_RANGE_500_DEG);
+    Serial.println("[  OK  ]");
+    Serial.print("Configuring IMU filter... ");
+    imu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+    Serial.println("[  OK  ]");
 }
 
 void loop() {
-    Serial.printf("potentiometer reading: %i\n", analogRead(TEST_POT));
+    int pot_val = analogRead(TEST_POT);
+    sensors_event_t a, g, trash;
+    imu.getEvent(&a, &g, &trash);
+    (void)trash;
+    Serial.println("Pot\tAX\tAY\tAZ\tGX\tGY\tGZ");
+    Serial.printf(
+        "%i\t%g\t%g\t%g\t%g\t%g\t%g\n",
+        pot_val,
+        a.acceleration.x, a.acceleration.y, a.acceleration.z,
+        g.gyro.x, g.gyro.y, g.gyro.z);
 }
