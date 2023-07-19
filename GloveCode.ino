e3ebfada09eb027628e23c92b3526f6bca8accc0
Camp Student <campstudent@SG4-3202-03.USG.LOCAL>
2023-07-19 13:10:09 -0400

    add task code

--- GloveCode.ino
+++ GloveCode.ino
@@ -1,12 +1,41 @@
+// https://github.com/MajicDesigns/MD_Menu/blob/main/examples/Menu_Test/Menu_Test_Disp.cpp
+
+#include <LiquidCrystal_PCF8574.h>
 #include <SensorFusion.h>
 #include <Adafruit_MPU6050.h>
 #include <Wire.h>
+#include "task.h"
 
 Adafruit_MPU6050 imu;
 SF sensor_fusion;
 
+LiquidCrystal_PCF8574 lcd(0x27);
+
 #define TEST_POT 36
 
+void accelUpdate(void*);
+Task accelTask("accelTask", 0, accelUpdate);
+void accelUpdate(void* arg) {
+    sensors_event_t a, g, trash;
+    imu.getEvent(&a, &g, &trash);
+    (void)trash;
+    sensor_fusion.MahonyUpdate(
+        g.gyro.x, g.gyro.y, g.gyro.z,
+        a.acceleration.x, a.acceleration.y, a.acceleration.z,
+        sensor_fusion.deltatUpdate());
+}
+
+void testLoop(void*);
+Task testTask("testTask", 500, testLoop);
+void testLoop(void* arg) {
+    int pot_val = analogRead(TEST_POT);
+
+    lcd.clear();
+    lcd.home();
+    lcd.printf("yaw: %3f", sensor_fusion.getYaw());
+}
+
+TaskManager mgr;
 void setup() {
     Serial.begin(115200);
     Serial.print("Connecting IMU...         ");
@@ -15,6 +44,10 @@ void setup() {
        ESP.restart();
     } else
         Serial.println("[  OK  ]");
+    Serial.print("Starting LCD...           ");
+    lcd.begin(16, 2);
+    lcd.setBacklight(255);
+    Serial.println("[  OK  ]");
     Serial.print("Configuring IMU accel...  ");
     imu.setAccelerometerRange(MPU6050_RANGE_8_G);
     Serial.println("[  OK  ]");
@@ -24,22 +57,14 @@ void setup() {
     Serial.print("Configuring IMU filter... ");
     imu.setFilterBandwidth(MPU6050_BAND_21_HZ);
     Serial.println("[  OK  ]");
+    Serial.print("Starting tasks...         ");
+    mgr.add(&accelTask);
+    mgr.add(&testTask);
+    Serial.println("[  OK  ]");
 }
 
+
+
 void loop() {
-    int pot_val = analogRead(TEST_POT);
-    sensors_event_t a, g, trash;
-    imu.getEvent(&a, &g, &trash);
-    (void)trash;
-    sensor_fusion.MahonyUpdate(
-        g.gyro.x, g.gyro.y, g.gyro.z,
-        a.acceleration.x, a.acceleration.y, a.acceleration.z,
-        sensor_fusion.deltatUpdate());
-    Serial.println("Pot\tAX\tAY\tAZ\tGX\tGY\tGZ\tpitch\troll\tyaw");
-    Serial.printf(
-        "%i\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\n",
-        pot_val,
-        a.acceleration.x, a.acceleration.y, a.acceleration.z,
-        g.gyro.x, g.gyro.y, g.gyro.z,
-        sensor_fusion.getPitch(), sensor_fusion.getRoll(), sensor_fusion.getYaw());
+    mgr.run_all();
 }
---
+++ task.h
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+#include <vector>
+
+typedef unsigned long ul;
+typedef void (*voidvoid)(void*);
+
+class Task {
+    public:
+    const char* name;
+    void* param;
+    const voidvoid handler;
+    ul last;
+    ul interval;
+    bool running;
+    Task(const char* name, ul interval, voidvoid fun, void* arg = NULL)
+    : name(name),
+      interval(interval),
+      handler(fun),
+      running(true),
+      param(arg) {
+    }
+    void run(bool debug = false) {
+        if (!this->running) {
+            if (debug) Serial.printf("%s task is stopped\n", this->name);
+            return;
+        }
+        ul now = millis();
+        if (now - this->last > this->interval) {
+            if (debug) Serial.printf("%s task is running\n", this->name);
+            this->last = now;
+            this->handler(this->param);
+        } else {
+            if (debug) Serial.printf("%s task is waiting\n", this->name);
+        }
+    }
+};
+
+class TaskManager {
+    std::vector<Task*> tasks;
+    public:
+    TaskManager() {}
+    void add(Task* task) {
+        this->tasks.push_back(task);
+    }
+    void run_all(bool debug = false) {
+        for (Task* t : this->tasks) t->run(debug);
+    }
+};
