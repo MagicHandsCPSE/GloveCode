d6fd6395073011058d1e6bf9a48c0cadeab2df34
Camp Student <campstudent@SG4-3202-03.USG.LOCAL>
2023-07-20 16:02:29 -0400

    reverse the code
    yet to see if it works

--- GloveCode.ino
+++ GloveCode.ino
@@ -4,16 +4,25 @@
 #include <SensorFusion.h>
 #include <Adafruit_MPU6050.h>
 #include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEServer.h>
 #include "task.h"
 
 /////////////////////////////
 // MAKE SURE THESE ARE SAME AS DRONE HAND CODE COPY PASTE
 #define DEVICE_NAME         "24f7ad15-fdde-4c83-8327-400a87de818c"
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define SERVO_CHAR_UUID     "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define SERVO1_CHAR_UUID    "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define SERVO2_CHAR_UUID    "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define SERVO3_CHAR_UUID    "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 /////////////////////////////
+
+static BLEUUID serviceUUID(SERVICE_UUID);
+static BLEUUID servo1CharacteristicUUID(SERVO1_CHAR_UUID);
+static BLEUUID servo2CharacteristicUUID(SERVO2_CHAR_UUID);
+static BLEUUID servo3CharacteristicUUID(SERVO3_CHAR_UUID);
+static BLERemoteCharacteristic* servo1Characteristic;
+static BLERemoteCharacteristic* servo2Characteristic;
+static BLERemoteCharacteristic* servo3Characteristic;
+static BLEAdvertisedDevice* glove;
 
 Adafruit_MPU6050 imu;
 SF sensor_fusion;
@@ -22,8 +31,53 @@ LiquidCrystal_PCF8574 lcd(0x27);
 
 #define TEST_POT 36
 
-BLECharacteristic* servoChar;
+bool oktoconnect = false;
+bool connected = false;
+class freezeOnDisconnect : public BLEClientCallbacks {
+    void onConnect(BLEClient* _) {
+    }
+    void onDisconnect(BLEClient* _) {
+        connected = false;
+    }
+};
 
+bool connect() {
+    lcd.clear();
+    lcd.home();
+    lcd.print("connecting...");
+    BLEClient* client = BLEDevice::createClient();
+    client->setClientCallbacks(new freezeOnDisconnect());
+    client->connect(glove);
+    client->setMTU(517); // maximum
+    BLERemoteService* service = client->getService(serviceUUID);
+    if (service == NULL) goto error;
+    servo1Characteristic = service->getCharacteristic(servo1CharacteristicUUID);
+    servo2Characteristic = service->getCharacteristic(servo2CharacteristicUUID);
+    servo3Characteristic = service->getCharacteristic(servo3CharacteristicUUID);
+    if (servo1Characteristic == NULL) goto error;
+    if (servo2Characteristic == NULL) goto error;
+    if (servo3Characteristic == NULL) goto error;
+    lcd.clear();
+    lcd.home();
+    lcd.print("connected!");
+    return true;
+error:
+    client->disconnect();
+    lcd.clear();
+    lcd.home();
+    lcd.print("error");
+    return false;
+}
+
+class chooseWithService : public BLEAdvertisedDeviceCallbacks {
+    void onResult(BLEAdvertisedDevice device) {
+        if (device.haveServiceUUID() && device.isAdvertisingService(serviceUUID)) {
+            BLEDevice::getScan()->stop();
+            glove = new BLEAdvertisedDevice(device);
+            oktoconnect = true;
+        }
+    }
+};
 
 Task accelTask("accelTask", 0, NULL, [](void* arg) -> void {
     sensors_event_t a, g, trash;
@@ -36,41 +90,26 @@ Task accelTask("accelTask", 0, NULL, [](void* arg) -> void {
 });
 
 Task sendservoTask("sendservoTask", 100, NULL, [](void* arg) -> void {
-    lcd.clear();
-    lcd.home();
     int servoPos = analogRead(TEST_POT);
     servoPos = map(servoPos, 0, 4095, 0, 180);
-    lcd.printf("servo: %i", servoPos);
-    // prepare for sending OK
-    lcd.setCursor(0, 1);
-
-    // Send it
-    servoChar->setValue(servoPos);
-    servoChar->notify();
+    // TODO add read code
+    uint8_t val = (uint8_t) servoPos;
+    servo1Characteristic->writeValue(val);
+    servo2Characteristic->writeValue(val);
+    servo3Characteristic->writeValue(val);
 });
 
-void setupBLE() {
-    BLEDevice::init(DEVICE_NAME);
-    BLEServer* server = BLEDevice::createServer();
-    BLEService* service = server->createService(SERVICE_UUID);
-    servoChar = service->createCharacteristic(
-                    SERVO_CHAR_UUID,
-                    BLECharacteristic::PROPERTY_READ |
-                    BLECharacteristic::PROPERTY_WRITE |
-                    BLECharacteristic::PROPERTY_NOTIFY
-                );
-    service->start();
-    BLEAdvertising* ad = BLEDevice::getAdvertising();
-    ad->addServiceUUID(SERVICE_UUID);
-    ad->setScanResponse(true);
-    ad->setMinPreferred(0x06);  
-    ad->setMaxPreferred(0x12);
-    BLEDevice::startAdvertising();
+void startScan() {
+    BLEDevice::init("");
+    BLEScan* scan = BLEDevice::getScan();
+    scan->setAdvertisedDeviceCallbacks(new chooseWithService());
+    scan->setInterval(1349);
+    scan->setWindow(449);
+    scan->setActiveScan(true);
+    scan->start(9999, false);
 }
 
-TaskManager mgr;
 void setup() {
-    Serial.begin(115200);
     // Connect IMU
     imu.begin();
     
@@ -83,15 +122,15 @@ void setup() {
     imu.setGyroRange(MPU6050_RANGE_500_DEG);
     imu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
-    setupBLE();
-
-    // Setup tasks
-    mgr.add(&accelTask);
-    mgr.add(&sendservoTask);
+    startScan();
 }
 
-
-
 void loop() {
-    mgr.run_all();
+    sendservoTask.run();
+    accelTask.run();
+    if (oktoconnect && !connected) {
+        connected = connect();
+        sendservoTask.running = connected;
+    }
+    yield();
 }
